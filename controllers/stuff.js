79850fe9ca600aa25b3cb8b2167412e4e011fa18
SECK Babacar <babacar.seck.external@airbus.com>
2025-03-25 00:45:01 +0100

    * Add middleware for auht and upload file

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,13 +1,20 @@
 const Thing = require('../models/Thing');
 
 exports.createThing = (req, res, next) => {
-  delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-    .then(() => res.status(201).json({message : 'Post saved successfully!'}))
-    .catch(error => res.status(400).json({error}));
+  console.log(` req : ${req}`)
+  const thingObject = JSON.parse(req.body.thing);
+  delete thingObject._id;
+  delete thingObject._userId;
+
+  console.log(`${req.file}`)
+  const thing = new Thing({
+    ...thingObject, 
+    userId: req.auth.userId,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  })
+  thing.save()
+  .then(() => res.status(201).json({message : 'Thing saved successfully!'}))
+  .catch(error => res.status(400).json({error}));
 }
 
 exports.modifyThing = (req, res, next) => {
